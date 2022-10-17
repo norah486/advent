8540437ddac2dc8c6d38f506b5a835dd104e07bf
norah486 <32691116+norah486@users.noreply.github.com>
2022-10-17 01:22:50 -0300

    tidy up the file, still some warnings :(

--- 2021/03/main.cpp
+++ 2021/03/main.cpp
@@ -4,29 +4,24 @@
 #include <vector>
 
 using namespace std;
-char mostRep(const vector<string>& values, int pos, int mode) {
+char mostRep(const vector<string>& values, int pos) {
     int counter0[20] = { 0 };
     int counter1[20] = { 0 };
 
     string currentValue;
-    for (int i = 0; i < values.size(); i++) {
-        currentValue = values[i];
+    // So like, it was complaining about this below, and it seems to just make tue values from vector a
+    // constant to not call it everytime, then the : values is the amount of values there are in the
+    // vector or the size of it, it's what I'm expecting it is, so I hope this is right
+    for (const auto & value : values) {
+        currentValue = value;
         for (int j = 0; j <= currentValue.length(); j++) {
             if (currentValue[j] == '0') counter0[j]++;
             else if (currentValue[j] == '1') counter1[j]++;
         }
     }
 
-    // Mode:
-    // 0 - Return '1' if number of elements is the same
-    // 1 - Return '0' if number of elements is the same
-    if (mode == 0) {
-        if (counter0[pos] > counter1[pos]) return '0';
-        else if (counter0[pos] <= counter1[pos]) return '1';
-    } else if (mode == 1) {
-        if (counter0[pos] > counter1[pos]) return '0';
-        else if (counter0[pos] <= counter1[pos]) return '1';
-    }
+    if (counter0[pos] > counter1[pos]) return '0';
+    else if (counter0[pos] <= counter1[pos]) return '1';
     return 'X';
 }
 
@@ -79,7 +74,7 @@ int main() {
     cout << "Epsilon Rate: " << counterLcc << endl;
 
     for (int j = 0; j < textLength; j++) {
-        char compareTo = mostRep(storedValues, j, 0);
+        char compareTo = mostRep(storedValues, j);
         string tempString;
         for (i = storedValues.size() - 1; i >= 0; i--) {
             tempString = storedValues[i];
@@ -91,7 +86,7 @@ int main() {
     }
 
     for (int j = 0; j < textLength; j++) {
-        char compareTo = mostRep(storedValuesCopy, j, 1);
+        char compareTo = mostRep(storedValuesCopy, j);
         string tempString;
         for (i = storedValuesCopy.size() - 1; i >= 0; i--) {
             tempString = storedValuesCopy[i];
